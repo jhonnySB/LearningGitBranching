cc67bd405464e29838722bbc5e0dc90996f43cc6
Jhon <tegchome123@gmail.com>
2022-10-23 19:07:50 -0500

    c3-main

--- src/main/java/com/main/MyApp.java
+++ src/main/java/com/main/MyApp.java
@@ -3,10 +3,11 @@ package com.main;
 public class MyApp {
 
 	int var;
+	int varMainBranch;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		varMainBranch = null;
 	}
 
 }
