f009cf9b476b4f8748144d76c0179f1ed9befc6e
Jhon <tegchome123@gmail.com>
2022-10-23 19:50:02 -0500

    c-main

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -1,11 +1,15 @@
 package com.main;
 
-public class MyApp {
+public class Main {
 
 	int var;
+	
 	static int varMainBranch;
 	
+	static String str;
+	
 	public static void main(String[] args) {
+		str = "";
 		varMainBranch = 0;
 		
 	}
