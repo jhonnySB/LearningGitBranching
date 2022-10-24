00b3dc2f9803366a7d6940d1a633baa2a202cec2
Jhon <tegchome123@gmail.com>
2022-10-23 20:03:17 -0500

    c-main

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -3,15 +3,15 @@ package com.main;
 public class Main {
 
 	int var;
-	
 	static int varMainBranch;
-	
 	static String str;
 	
 	public static void main(String[] args) {
 		str = "";
 		varMainBranch = 0;
-		
+		str.concat("hello world");
+		// ...
+		// ...
 	}
 
 	
